f27f1671953e52d9e5670cf66779eb109657bddf
jhuynh24 <jhuynh1735@gmail.com>
2020-10-01 19:21:13 -0500

    Added arrays for each type of character so the generator knows which characters to use.

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 // Connects the generate ID in th HTML document to the .js code. Runs the .js when the button is clicked.
 var generateBtn = document.querySelector("#generate");
 
-    
+// Code for the arrays defining the characters that will be used in the generator. 
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbersArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var special = ["!", "@", "#", "$", "%", "^", "&", "*", "=", "(", ")", "'", "+", ",", ".", "-", "/", ":", ";", "<", ">", "?", "[", "]", "|", "_", "`", "{", "}", "~"];   
 
 function generatePassword() {
   var lengthOfPassword = parseInt(prompt("How many characters would you like your password to be? (Min=8; Max=128)"));
